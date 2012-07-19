34b84ed6bac172fa8b5710bbae0cf37f2de35a37
Rene Schweiger <rschweiger@gmx.de>
2012-07-19 11:39:29 +0200

    initial commit leeres rails-Projekt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LiquidOrg::Application.config.secret_token = '38d6025e0bbaaeb4470cb59318c301d48bc0bf5fbdb0d510017bc8a9e698f61193d03874fb594eb6b3092f387a8e4b33b080f3a7e96c2280a6694191406f57d8'
